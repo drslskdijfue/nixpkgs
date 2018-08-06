5eecd1f4e9aa069a2cc08c417e932207ba4fcf0a
drslskdijfue <41970256+drslskdijfue@users.noreply.github.com>
2018-08-06 18:21:36 -0400

    Update linux-4.14.nix

--- pkgs/os-specific/linux/kernel/linux-4.14.nix
+++ pkgs/os-specific/linux/kernel/linux-4.14.nix
@@ -3,7 +3,7 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "4.14.61";
+  version = "4.14.24";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStrings (intersperse "." (take 3 (splitString "." "${version}.0"))) else modDirVersionArg;
